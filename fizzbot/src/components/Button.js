899f3d33180ebc409de94f2f060f4f5d960fd939
msnorma <msnormaline@gmail.com>
2019-06-21 00:03:30 +0200

    feat: adding needed folders for the API

---
+++ fizzbot/src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Button extends Component  {
+  render(){
+    return (
+      <button type="button" class="btn btn-outline-warning">Ask Away</button>
+    )
+  }
+}
+
+export default Button;
