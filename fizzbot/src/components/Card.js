3b5718186a75c5252b79612b1c4b168bb01aabc3
msnorma <msnormaline@gmail.com>
2019-06-23 12:03:50 +0200

    feat: show the message data from the API on the card element

--- fizzbot/src/components/Card.js
+++ fizzbot/src/components/Card.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 
 class Card extends Component  {
+
+//This object will hold in our data thats waiting to be rendered, stores output from api call
+  constructor() {
+    super();
+
+    this.state = {
+      message: '',
+      nextQuestion: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch(`https://api.noopschallenge.com/fizzbot`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          message: data.message,
+          nextQuestion: data.nextQuestion }) 
+          console.log(data.message)
+      },
+    );
+  }
+
   render(){
     return (
       <div className="card">
         <div className="card-body">
-          This is some text within a card body.
+          {this.state.message}
         </div>
       </div>
     )
