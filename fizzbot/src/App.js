f7266828354ee72e67ee4d681d7b5171cc77f87d
msnorma <msnormaline@gmail.com>
2019-06-23 10:47:28 +0200

    feat: importing the button and card components

--- fizzbot/src/App.js
+++ fizzbot/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import Button from "./components/Button";
+import Card from "./components/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <Button></Button>
-    </div>
-  );
+class App extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <Card/>
+        <Button/>
+      </div>
+    );
+  }
 }
 
 export default App;
