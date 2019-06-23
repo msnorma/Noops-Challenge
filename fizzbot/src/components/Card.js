b9edd5b74675af6eec09f5c224ed75ea746090bc
msnorma <msnormaline@gmail.com>
2019-06-23 10:46:55 +0200

    feat: card for displaying message

--- fizzbot/src/components/Card.js
+++ fizzbot/src/components/Card.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Card extends Component  {
   render(){
     return (
-      <div class="card">
-        <div class="card-body">
+      <div className="card">
+        <div className="card-body">
           This is some text within a card body.
         </div>
       </div>
