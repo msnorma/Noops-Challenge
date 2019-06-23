1accbaf98fca5d1c83dde119201f3448aec0cf9a
msnorma <msnormaline@gmail.com>
2019-06-23 10:46:15 +0200

    feat: testing api data and displaying on console

--- fizzbot/src/components/Button.js
+++ fizzbot/src/components/Button.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
 class Button extends Component  {
+  
+    //This object will hold in our data thats waiting to be rendered, stores output from api call
+    constructor() {
+      super();
+  
+      this.state = {
+        message: '',
+        nextQuestion: '',
+      };
+    }
+  
+    componentDidMount() {
+      fetch(`https://api.noopschallenge.com/fizzbot`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          message: data.message,
+          nextQuestion: data.nextQuestion }) 
+          console.log(data)
+      }, 
+    );
+  }
+
+
   render(){
     return (
-      <button type="button" class="btn btn-outline-warning">Ask Away</button>
+      <div>
+        <button onClick = {this.message} type="button" className="btn btn-outline-warning">Ask Away</button>
+      </div>
     )
   }
 }
